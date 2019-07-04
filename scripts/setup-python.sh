daadc09a630b079f88fbc219eadd00682a062c3b
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2019-07-04 17:07:45 +0100

    Note that python setup script is incomplete currently

--- scripts/setup-python.sh
+++ scripts/setup-python.sh
@@ -1,3 +1,5 @@
 #!/bin/ash
-echo "Creating link from /mnt/HD/HD_a2/nas-sysroot/usr/local/lib/python3.7 to /usr/local/lib/python3.7..."
+#TODO currently doesn't fix pip, more investigation required.
+echo "Creating links from /mnt/HD/HD_a2/nas-sysroot/usr/local/lib/python3.7 to /usr/local/lib/python3.7 and /usr/lib/python3.7..."
 ln -s /mnt/HD/HD_a2/nas-sysroot/usr/local/lib/python3.7 /usr/local/lib/python3.7
+ln -s /mnt/HD/HD_a2/nas-sysroot/usr/local/lib/python3.7 /usr/lib/python3.7
