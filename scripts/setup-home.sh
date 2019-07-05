1437e66d6fb7fc5c9fcd11c4d841cd3d92d85c3f
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2019-07-05 10:09:08 +0100

    Fix TMPDIR setting, fixing pip

--- scripts/setup-home.sh
+++ scripts/setup-home.sh
@@ -1,4 +1,10 @@
 #!/bin/ash
+echo "Deleting and Re-creating temporary directory..."
+
+rm -rv /mnt/HD/HD_a2/.temp
+
+mkdir /mnt/HD/HD_a2/.temp
+
 echo "Making home directory..."
 
 mkdir -p /home/admin
@@ -9,6 +15,9 @@ cat << "EOF" > /home/admin/.bashrc
 #Set up PATH and LD_LIBRARY_PATH
 export PATH="/mnt/HD/HD_a2/nas-sysroot/usr/local/bin:/mnt/HD/HD_a2/nas-sysroot/usr/local/sbin:$PATH"
 export LD_LIBRARY_PATH="/mnt/HD/HD_a2/nas-sysroot/lib:/mnt/HD/HD_a2/nas-sysroot/usr/local/lib:$LD_LIBRARY_PATH"
+
+#Set up TMPDIR
+export TMPDIR=/mnt/HD/HD_a2/.temp
 EOF
 
 echo "Creating .profile..."
