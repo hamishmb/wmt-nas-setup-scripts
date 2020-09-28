7d85b333ade44a6bf45f0cf184617cffb3e9f514
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2020-09-28 10:33:25 +0100

    Try a different way of editing crontab that seems more likely to work

--- scripts/setup-autobackup.sh
+++ scripts/setup-autobackup.sh
@@ -16,4 +16,4 @@
 #---------- MAKE CUSTOM BINARIES AVAILABLE IN THIS SCRIPT ----------
 . /home/admin/.profile
 
-echo (crontab -l ; echo "0 11 * * 0 /mnt/HD/HD_a2/backupandrestore/init_backup.sh") | crontab -
+echo "0 11 * * 0 /mnt/HD/HD_a2/backupandrestore/init_backup.sh" >> /var/spool/crontabs/root
