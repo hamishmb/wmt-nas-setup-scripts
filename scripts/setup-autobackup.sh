0e04f22c80156b10508d4cc89dfbceec0b36f768
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2020-09-28 10:27:14 +0100

    Add autobackup script to repo

---
+++ scripts/setup-autobackup.sh
@@ -0,0 +1,19 @@
+#!/bin/ash
+# NAS box setup scripts.
+# Copyright (C) 2019-2020 Wimborne Model Town
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 or,
+# at your option, any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#---------- MAKE CUSTOM BINARIES AVAILABLE IN THIS SCRIPT ----------
+. /home/admin/.profile
+
+echo (crontab -l ; echo "0 1 * * 0 /mnt/HD/HD_a2/backupandrestore/init_backup.sh") | crontab -
